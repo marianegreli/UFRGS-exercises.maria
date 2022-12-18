d69d506fcf944bd4b680b79209c92d55c9b7bcab
Maria Fernanda Negreli <78988198+marianegreli@users.noreply.github.com>
2022-12-18 15:21:23 -0300

    Update and rename EXTRA/exemplo3.c to EXTRA/22.12.18/TABELA_ASCII.c

--- EXTRA/22.12.18/TABELA_ASCII.c
+++ EXTRA/22.12.18/TABELA_ASCII.c
@@ -1,9 +1,12 @@
 // Faça um programa que imprima a tabela ASCII de A até z,
 // mostrando o caractere em bases octal, decimal e hexadecimal.#include <stdio.h>
+#include<stdio.h>
 
 int main() {
 char letra;
+  
 for (letra = 'A'; letra <= 'z'; letra++)
 printf("[%c] %3o %3d %3x\n", letra, letra, letra, letra);
+  
 return 0;
 }
