f358a87ab55871ae32c5d202cc70ff87af10051b
Maria Fernanda Negreli <78988198+marianegreli@users.noreply.github.com>
2022-12-18 15:24:16 -0300

    Update and rename EXTRA/fatorial2.c to EXTRA/22.12.18/FACTORIAL_calculator2.c

--- EXTRA/22.12.18/FACTORIAL_calculator2.c
+++ EXTRA/22.12.18/FACTORIAL_calculator2.c
@@ -1,10 +1,15 @@
-// tentando por uma solução mais facil pq pelo amor
+// tentando outra solução para o calculo de fatorial de um nuemro inteiro positivo informado
 
 #include<stdio.h>
 void main(){
+    
     int i, numero;
+    
     printf("Entre com um numero: ");
     scanf("%d", &numero);
+    
+    // processamento 
+    
     for ((i = numero-1); i>=1; i--){
         numero = numero*i;
     }
